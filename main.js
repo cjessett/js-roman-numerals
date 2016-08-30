c11bc51cb617e82a4fa15aa7270c1b1526c58e51
CJ Jessett <cjessett@gmail.com>
2016-08-30 14:27:20 -0500

    initial

---
+++ main.js
@@ -0,0 +1,31 @@
+(function () {
+  var idName = "userInput";
+  var previewId = "preview";
+
+  function attachEvent(idName) {
+    var userInput = document.getElementById(idName);
+    var preview = document.getElementById(previewId);
+
+    userInput.onkeyup = function () {
+      preview.innerHTML = toRoman(this.value);
+    }
+  };
+  attachEvent(idName);
+})();
+
+function toRoman(numString) {
+  var nums = { 1000:'M', 900:'CM', 500:'D', 400:'CD',100:'C', 90:'XC', 50:'L', 40:'XL', 10:'X',9:'IX', 5:'V', 4:'IV', 1:'I' }
+  var result = "";
+  var keys = Object.keys(nums);
+  var i = keys.length - 1;
+
+  while(i >= 0) {
+    var int = keys[i]
+    while(numString >= parseInt(int)) {
+      numString -= int;
+      result += nums[int];
+    }
+    i--;
+  }
+  return result
+}
